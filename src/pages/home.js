c9bedebd256125d35fdc65c150eb538c0608bba8
Brent Haskins <brenthaskins@icloud.com>
2019-09-17 11:53:37 -0700

    Updating files

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import thisNavbar from "../components/navbar/thisNavbar";
+
+function Home() {
+    return (
+        <div>
+          <thisNavbar />
+        </div>
+      );
+    }
+
+export default Home;
