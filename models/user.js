df74e3a5f58361b1b71dfed27c03bb6c350a7dfe
JayG <jaypguevara@gmail.com>
2019-09-22 07:52:33 -0700

    commit

--- models/user.js
+++ models/user.js
@@ -3,16 +3,20 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    phonenumber: { type: INT, required: true },
+    birthday: { type: Date, required: true },
     bio: { type: String },
     skills: { type: Array },
     date: { type: Date, default: Date.now },
     posts: [{
         type: Schema.Types.ObjectId,
         ref: "Post"
+        
     }
     ],
-    email:{type:String},
-    password:{type:String},
+    isFollowed: false
 
 
 });
