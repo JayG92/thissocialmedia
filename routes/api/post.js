82819914851b30cf1a3459c08575637edb416478
JayG <jaypguevara@gmail.com>
2019-09-27 14:38:21 -0700

    fix require statments

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const postController = require("../../controllers/postController");
+const postController = require("../../controllers/postcontroller");
 
 // /api/post
 router.route("/")
