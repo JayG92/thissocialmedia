3588b2d263383fd4ada9bb538ac08f962009a73f
JayG <jaypguevara@gmail.com>
2019-09-27 14:59:58 -0700

    added server stuff

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(require("./routes/api"));
 
+app.get("*", function(req, res){
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+})
+
 // Start the API server
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
